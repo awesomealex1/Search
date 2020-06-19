8dbb82eff1f0cf9bf7c4730181e29ec2ff886432
Alex Murphy <lordpizza11@gmail.com>
2020-06-19 17:25:48 +0200

    Fixed select in toolbar

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -5,20 +5,30 @@ class Toolbar extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            algorithm: "BFS",   //Defaultvalue in select
+        }
+        this.handleAlgorithmChange = this.handleAlgorithmChange.bind(this);
+    }
+
+    handleAlgorithmChange(event) {
+        this.setState({
+            algorithm: event.target.value,
+        });
     }
 
     render() {
         return (
             <div>
-                <button onClick={() => this.props.handleSearch(1,"BFS")}>Search</button>
-                <select>
+                <button onClick={() => this.props.handleSearch(1,this.state.algorithm)}>Search</button>
+                <select defaultValue={this.state.algorithm} onChange={this.handleAlgorithmChange}>
                     <option value="DFS">DFS</option>
                     <option value="BFS">BFS</option>
                 </select>
                 <label>Square Size:</label>
-                <input type="range" min="1" max="10" value="2"></input>
+                <input type="range" min="1" max="10" defaultValue="2"></input>
                 <label>Speed:</label>
-                <input type="range" min="1" max="10" value="2"></input>
+                <input type="range" min="1" max="10" defaultValue="2"></input>
             </div>
         );
     }
