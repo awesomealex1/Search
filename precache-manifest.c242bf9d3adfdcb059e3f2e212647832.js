e73e77a4b7a09e452ebb4ba2bd37f920e8bb1d7a
Alex Murphy <lordpizza11@gmail.com>
2020-07-20 23:59:59 +0200

    Updates

---
+++ precache-manifest.c242bf9d3adfdcb059e3f2e212647832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3650f1da29968206aab9008309a208f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9700d3ed869b873ef202",
+    "url": "/static/css/main.e2b66daf.chunk.css"
+  },
+  {
+    "revision": "ece58dafdca16c864feb",
+    "url": "/static/js/2.7c5b46e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c5b46e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9700d3ed869b873ef202",
+    "url": "/static/js/main.e291e748.chunk.js"
+  },
+  {
+    "revision": "8b6b5af62156e640a503",
+    "url": "/static/js/runtime-main.c6ad54f0.js"
+  }
+]);
